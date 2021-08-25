77e5191925f87b91f75be4a60af2f99db70ff2e7
ithe148 <ilya.rendakov94@gmail.com>
2021-08-25 13:42:33 +0300

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -4,5 +4,4 @@ yum install -y epel-release && echo "install epel" >> /var/log/azure/install.log
 yum update -y && echo "update all" >> /var/log/azure/install.log
 yum install -y nginx && echo "install nginx" >> /var/log/azure/install.log
 systemctl daemon-reload && echo "daemon-reload" >> /var/log/azure/install.log
-nginx -t >> /var/log/azure/install.log
 systemctl enable --now nginx && echo "enable and run nginx" >> /var/log/azure/install.log
