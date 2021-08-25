a452ac07cd2767621498c4b80ba87eefcc3fba2f
ithe148 <ilya.rendakov94@gmail.com>
2021-08-25 15:03:10 +0300

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -5,3 +5,6 @@ yum update -y && echo "update all" >> /var/log/azure/install.log
 yum install -y nginx && echo "install nginx" >> /var/log/azure/install.log
 systemctl daemon-reload && echo "daemon-reload" >> /var/log/azure/install.log
 systemctl enable --now nginx && echo "enable and run nginx" >> /var/log/azure/install.log
+
+
+exit 0 
