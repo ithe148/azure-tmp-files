e97e0c171113595f467c5634d6fff2bee6e8a06d
ithe148 <89517577+ithe148@users.noreply.github.com>
2021-08-26 10:17:02 +0300

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-yum install -y epel-release && echo "install epel" >> /var/log/azure/install.log
-yum update -y && echo "update all" >> /var/log/azure/install.log
-yum install -y nginx && echo "install nginx" >> /var/log/azure/install.log
-systemctl daemon-reload && echo "daemon-reload" >> /var/log/azure/install.log
-systemctl enable --now nginx && echo "enable and run nginx" >> /var/log/azure/install.log
-echo "Nginx was installed" >> /var/log/azure/install.log
+echo "--------------------------------" >>  /var/log/azure/install.log
+echo "START INSTALL NGINX"
+echo "START INSTALL NGINX" >> /var/log/azure/install.log
+echo "install epel" >> /var/log/azure/install.log
+yum install -y epel-release
+echo "epel was installed " >> /var/log/azure/install.log
+echo "update all" >> /var/log/azure/install.log
+yum update -y
+echo "system was updated" >> /var/log/azure/install.log
+echo "install nginx" >> /var/log/azure/install.log
+yum install -y nginx
+echo "nginx was installed" >> /var/log/azure/install.log
+echo "daemon-reload" >> /var/log/azure/install.log
+systemctl daemon-reload
+echo "daemon-reloaded" >> /var/log/azure/install.log
+echo "enable and run nginx" >> /var/log/azure/install.log
+systemctl enable --now nginx
+echo "Nginx was runned" >> /var/log/azure/install.log
 
-echo "Nginx was installed"
+echo "END" >> /var/log/azure/install.log
+
+echo "nginx was installed"
+
 
 
 
