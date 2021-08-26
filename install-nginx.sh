71e18a38f276d866ab644dcad7a2d504ec50e938
ithe148 <89517577+ithe148@users.noreply.github.com>
2021-08-26 10:37:25 +0300

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -6,9 +6,6 @@ echo "START INSTALL NGINX" >> /var/log/azure/install.log
 echo "install epel" >> /var/log/azure/install.log
 yum install -y epel-release
 echo "epel was installed " >> /var/log/azure/install.log
-echo "update all" >> /var/log/azure/install.log
-yum update -y
-echo "system was updated" >> /var/log/azure/install.log
 echo "install nginx" >> /var/log/azure/install.log
 yum install -y nginx
 echo "nginx was installed" >> /var/log/azure/install.log
